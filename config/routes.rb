1c4679225c0c97de14bb8aabba45c544ca6ca389
horiuchi67 <1234mhoriuchi@gmail.com>
2020-06-01 18:03:20 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/about' => 'home#about', as: 'about'
+  root "home#top"
+  devise_for :users
+  
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :users, only: [:show, :index, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
